e21517661494d0844897d3fafaff788e7d9f5bcf
Therail <98039185+TherailSlayer@users.noreply.github.com>
2023-04-02 22:20:50 +0700

    Add files via upload

---
+++ source/scripts/carousel.js
@@ -0,0 +1,24 @@
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("items");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
+
